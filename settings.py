de58f502f8897e50617fc756651ca98f39314194
Ming Fang <mingfang@mac.com>
2022-03-05 21:19:32 +0000

    configure using ENV

--- settings.py
+++ settings.py
@@ -2,31 +2,20 @@ import json
 import os
 
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://user:user@localhost:27017/eve')
-
-# Enable reads (GET), inserts (POST) and DELETE for resources/collections
-# (if you omit this line, the API will default to ['GET'] and provide
-# read-only access to the endpoint).
-RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
-
-# Enable reads (GET), edits (PATCH) and deletes of individual items
-# (defaults to read-only item access).
-ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
-
-RENDERERS = [
-    'eve.render.JSONRenderer'
-]
-
-# We enable standard client cache directives for all resources exposed by the
-# API. We can always override these global settings later.
-CACHE_CONTROL = 'max-age=20'
-CACHE_EXPIRES = 20
-
 DOMAIN_PATH = os.environ.get('DOMAIN_PATH', 'domain.json')
 with open(DOMAIN_PATH, "r") as jsonfile:
     DOMAIN = json.load(jsonfile)
 
-SCHEMA_ENDPOINT = '_schema'
-ALLOW_UNKNOWN = True
-ENFORCE_IF_MATCH = False
-URL_PREFIX = '/'
-VERSIONING = True
+ALLOW_UNKNOWN = os.environ.get('ALLOW_UNKNOWN', True)
+CACHE_CONTROL = os.environ.get('CACHE_CONTROL', 'max-age=20')
+CACHE_EXPIRES = os.environ.get('CACHE_EXPIRES', 20)
+ENFORCE_IF_MATCH = os.environ.get('ENFORCE_IF_MATCH', False)
+ITEM_METHODS = json.loads(os.environ.get('ITEM_METHODS', '["GET", "PATCH", "PUT", "DELETE"]'))
+OPLOG = os.environ.get('OPLOG', True)
+OPLOG_ENDPOINT = os.environ.get('OPLOG_ENDPOINT', '_oplog')
+RESOURCE_METHODS = json.loads(os.environ.get('RESOURCE_METHODS', '["GET", "POST", "DELETE"]'))
+RENDERERS = json.loads(os.environ.get('RENDERERS', '["eve.render.JSONRenderer"]'))
+SCHEMA_ENDPOINT = os.environ.get('SCHEMA_ENDPOINT', '_schema')
+URL_PREFIX = os.environ.get('URL_PREFIX', '/')
+VERSIONING = os.environ.get('VERSIONING', True)
+
